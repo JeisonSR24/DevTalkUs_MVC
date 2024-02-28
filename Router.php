2593f7e986d90afa5b0ad388407a639108edd59c
Jeison R <powermax242m@gmail.com>
2024-02-27 19:56:15 -0500

    Changes

--- Router.php
+++ Router.php
@@ -44,17 +44,25 @@ class Router
 
     public function render($view, $datos = [])
     {
-
-        // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
-            $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
+            $$key = $value; 
         }
 
-        ob_start(); // Almacenamiento en memoria durante un momento...
+        ob_start(); 
 
-        // entonces incluimos la vista en el layout
         include_once __DIR__ . "/views/$view.php";
+
         $contenido = ob_get_clean(); // Limpia el Buffer
-        include_once __DIR__ . '/views/layout.php';
+
+        // Utilizar el Layout de acuerdo a la URL
+        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+
+        if(str_contains($url_actual, '/admin')) {
+            include_once __DIR__ . '/views/admin-layout.php';
+        } else {
+            include_once __DIR__ . '/views/layout.php';
+        }
+
+        
     }
 }
